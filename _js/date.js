707e2475bf9f8b0a0d8ea2b6d3a62f61cac5eebb
unknown <toto@t.fr>
2023-08-04 16:12:16 +0200

    nombre_mystère

---
+++ _js/date.js
@@ -0,0 +1,18 @@
+"use strict;"
+
+console.log("date.js chargé");
+
+function sayMessage(msg) 
+{
+    console.log(msg);
+}
+
+function sayHello() 
+{
+    sayMessage("Début du programme 'Nombre mystère'");
+}
+
+function sayGoodbye() 
+{
+    sayMessage("Fin du programme 'Nombre mystère'");
+}
